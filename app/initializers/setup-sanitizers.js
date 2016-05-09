4f90591fb69929b0dc06bb140362bc4c73573db5
Anna Andresian <annaa@altschool.com>
2016-05-09 09:59:46 -0700

    Modify setup-sanitizers initializer for compatibility with Ember 1.13

--- app/initializers/setup-sanitizers.js
+++ app/initializers/setup-sanitizers.js
@@ -4,6 +4,12 @@ export default {
   name: 'ember-sanitize-setup-sanitizers',
 
   initialize: function(container) {
-    container.registerOptionsForType('sanitizer', { instantiate: false });
+    if (container.registerOptionsForType) {
+      // Ember 2.1+
+      container.registerOptionsForType('sanitizer', { instantiate: false });
+    } else {
+      // Ember 1.13
+      container.optionsForType('sanitizer', { instantiate: false });
+    }
   }
 };
