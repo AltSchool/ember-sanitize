c336966caeaad7a62a5a8ef4c7688442f6bb832e
Anna Andresian <annaa@altschool.com>
2016-05-10 14:19:04 -0700

    Define setup-sanitizers initializer in addon directory

---
+++ addon/initializers/setup-sanitizers.js
@@ -0,0 +1,13 @@
+export default {
+  name: 'ember-sanitize-setup-sanitizers',
+
+  initialize: function(container) {
+    if (container.registerOptionsForType) {
+      // Ember 2.1+
+      container.registerOptionsForType('sanitizer', { instantiate: false });
+    } else {
+      // Ember 1.13
+      container.optionsForType('sanitizer', { instantiate: false });
+    }
+  }
+};
--- app/initializers/setup-sanitizers.js
+++ app/initializers/setup-sanitizers.js
@@ -1,15 +1,1 @@
-import Ember from 'ember';
-
-export default {
-  name: 'ember-sanitize-setup-sanitizers',
-
-  initialize: function(container) {
-    if (container.registerOptionsForType) {
-      // Ember 2.1+
-      container.registerOptionsForType('sanitizer', { instantiate: false });
-    } else {
-      // Ember 1.13
-      container.optionsForType('sanitizer', { instantiate: false });
-    }
-  }
-};
+export { default } from 'ember-sanitize/initializers/setup-sanitizers';
